e5208e8f72ba2e0ad942b7719c404f0ca4903dca
mwasee68 <mwasee68@gmail.com>
2017-11-29 19:05:16 +0530

    new comment

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="practise.css" rel="stylesheet" type="text/css"/>
+        <title></title>
+    </head>
+    <body>
+        <div id="menu">
+           <?php include_once 'menu.php'; ?>
+        </div>
+        <div id="slider">
+            <?php include_once 'slider.php'; ?>
+        </div>
+        <div id="content">
+            <?php include_once 'content.php';?>
+        </div>
+        <div id="image">
+            <?php include_once 'image.php';?>
+        </div>
+        <div id="footer">
+            Licensed by mohd waseem
+        </div>
+        
+        <script src="js/jquery-3.2.1.slim.min.js" type="text/javascript"></script>
+        <script src="js/popper.min.js" type="text/javascript"></script>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+        
+        
+    </body>
+</html>
